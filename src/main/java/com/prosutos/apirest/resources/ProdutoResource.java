0f9ede79c04556f0bdca0e0a9f43025fe6c4ba53
Renato Welinton Schlogel <rschlogel@hotmail.com>
2019-06-05 01:05:12 -0300

    swagger

--- src/main/java/com/prosutos/apirest/resources/ProdutoResource.java
+++ src/main/java/com/prosutos/apirest/resources/ProdutoResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,21 +17,28 @@ import org.springframework.web.bind.annotation.RestController;
 import com.prosutos.apirest.models.Produto;
 import com.prosutos.apirest.repository.ProdutoRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 
 
 @RestController
 @RequestMapping(value="/api")
+@Api(value="API REST de Produtos")
+@CrossOrigin(origins="*")  // qualquer domino pode acessar
 public class ProdutoResource {
 	
 	@Autowired
 	ProdutoRepository produtoRepository;
 	
 	@GetMapping("/produto")
+	@ApiOperation(value="Retorna uma lista de produtos")
 	public List<Produto> listarProdutos(){
 		return produtoRepository.findAll();
 	}
 	
 	@GetMapping("/produto/{id}")
+	@ApiOperation(value="Retorna o produto com a chave passada")
 	public Produto buscarProduto(@PathVariable(value="id") Long id){
 		Optional<Produto> optionalProduto = produtoRepository.findById(id);
 		if(optionalProduto.isPresent()) {
@@ -41,11 +49,13 @@ public class ProdutoResource {
 	}
 	
 	@PostMapping("/produto")
+	@ApiOperation(value="Cria um novo produto")
 	public Produto salvar(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
 	}
 	
 	@DeleteMapping("/produto")
+	@ApiOperation(value="Atualiza um produto")
 	public void delete(@RequestBody Produto produto) {
 		produtoRepository.delete(produto);
 	}
